5f3c0c2d1897c0b6b9893e2ee3decd18cdc36e3c
yejine98 <yejine98@naver.com>
2021-10-14 16:24:22 +0900

    Initial commit

---
+++ src/main/java/com/example/pangyapangya/PangyapangyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pangyapangya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangyapangyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PangyapangyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pangyapangya/PangyapangyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pangyapangya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangyapangyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
