68d2ff5dd4ae27a63c196e8a1ee2f4819e9ef760
yejine98 <yejine98@naver.com>
2021-10-16 02:01:59 +0900

    템플릿 컨트롤러 연결완료, static 폴더 스타일 적용완료

---
+++ src/main/java/com/example/pangyapangya/controller/mainController.java
@@ -0,0 +1,30 @@
+package com.example.pangyapangya.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/main/*")
+@RequiredArgsConstructor
+public class mainController {
+
+    @GetMapping("index")
+    public String index(){
+        return "main/index";
+    }
+
+    @GetMapping("elements")
+    public String elements(){
+        return "main/elements";
+    }
+
+    @GetMapping("generic")
+    public String generic(){
+        return "main/generic";
+    }
+
+}
