7aba6b5bd23a71c640a42399db05870ee012f7c9
yejine98 <yejine98@naver.com>
2021-10-18 17:35:47 +0900

    css 파일, image파일 경로 수정완료/ 오류 완전 해결!

--- src/main/java/com/example/pangyapangya/config/MvcConfiguration.java
+++ src/main/java/com/example/pangyapangya/config/MvcConfiguration.java
@@ -13,9 +13,9 @@ public class MvcConfiguration implements WebMvcConfigurer {
         // 해당 폴더안에 안에 안에 있는 모든 파일을 찾기
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60*60*24*365);
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").setCachePeriod(60*60*24*365);
-        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60*60*24*365);
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/").setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/sass/**").addResourceLocations("classpath:/static/sass/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/").setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/webfonts/**").addResourceLocations("classpath:/static/webfonts/").setCachePeriod(60 * 60 * 24 * 365);
     }
 }
