bbe31e76a97ef944a943006e82496398e632c6be
han <wnsrbod@naver.com>
2021-10-27 14:51:53 +0900

    마이페이지(사장님) 커밋

---
+++ src/main/java/com/example/pangyapangya/beans/vo/CeoVO.java
@@ -0,0 +1,19 @@
+package com.example.pangyapangya.beans.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class CeoVO {
+    private String ceoId;
+    private String ceoPw;
+    private String ceoName;
+    private String shopName;
+    private String shopCallNumber;
+    private String shopZipcode;
+    private String shopAddress;
+    private String shopAddressDetail;
+    private String shopPhoto;
+    private String phoneNum;
+}
