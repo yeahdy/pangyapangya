83570bc81a62a0bb98a2ee25530d5fd4dacd7a5c
gomjiyun <rlawldbs7770@gmail.com>
2021-10-28 17:13:24 +0900

    test file 작업완료

--- src/main/java/com/example/pangyapangya/mappers/CartMapper.java
+++ src/main/java/com/example/pangyapangya/mappers/CartMapper.java
@@ -8,5 +8,17 @@ import java.util.List;
 @Mapper
 public interface CartMapper {
     //장바구니 목록 가져오기
-    public List<CartVO> getCartList();
+    public List<CartVO> getList();
+    //장바구니 목록 가져오기
+    public List<CartVO> getCartList(String userId);
+    //장바구니 추가하기
+    public void insertCart(CartVO cart);
+    //장바구니 추가(pk가져오기)
+    public void insertSelect_cartNum(CartVO cart);
+    //장바구니 삭제
+    public int deleteCart(Long cartNum);
+    //수량(breadCnt) 수정
+    public boolean updateBreadCnt(Long cartNum, int breadCnt);
+    //회원탈퇴 시 장바구니
+    public boolean leaveUser_cart(String userId);
 }
