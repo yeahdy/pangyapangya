4ecd0b6affae54ffe8c094885c80c6ebee202018
yejine98 <yejine98@naver.com>
2021-10-29 15:03:21 +0900

    회원가입 (사장님, 일반회원) 아이디 중복확인 완료, 비밀번호 확인 완료, 비밀번호 암호화 의존성 추가

--- src/main/java/com/example/pangyapangya/controller/UserRestAPIController.java
+++ src/main/java/com/example/pangyapangya/controller/UserRestAPIController.java
@@ -1,13 +1,9 @@
 package com.example.pangyapangya.controller;
 
-import com.example.pangyapangya.beans.vo.UserVO;
 import com.example.pangyapangya.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Random;
 
 /*
     [ Task ]		    [ URL ]			    [ Method ]		[ Parameter ]			    [ Form ]	[ URL이동 ]
@@ -22,50 +18,18 @@ import java.util.Random;
     회원가입- 사장님	/user/joinCEO2		POST		    ceoVO				        있음      	이동
     회원가입- 약관동의	/user/joinConfirm	X		        X				            없음      	없음
 */
-@Controller
+@RestController
 @Slf4j
 @RequestMapping("/user/*")
 @RequiredArgsConstructor
-public class UserController {
+public class UserRestAPIController {
     private final UserService userService;
 
-    /* 로그인 */
-    @GetMapping("login")
-    public String login(){ return "user/login"; }
-
-
-    /* 아이디 찾기 */
-    @GetMapping("idFind")
-    public String idFind(){ return "user/idFind"; }
-
-    /* 아이디 찾기 완료 */
-    @GetMapping("idFindSuccess")
-    public String idFindSuccess(){ return "user/idFindSuccess"; }
-
-    /* 비밀번호 찾기 */
-    @GetMapping("pwFind")
-    public String pwFind(){ return "user/pwFind"; }
-
-    /* 비밀번호 찾기 완료 */
-    @GetMapping("pwFindSuccess")
-    public String pwFindSuccess(){ return "user/pwFindSuccess"; }
-
-    /* 회원가입- 일반회원 */
-    @GetMapping("join")
-    public String join(UserVO userVO){
-        userService.join(userVO);
-        return "user/join";
+    /* 아이디 중복검사 */
+    @GetMapping("/idCheck/{userId}")
+    public String checkId(@PathVariable String userId){
+        boolean check= userService.checkId(userId);
+        return check + "";
     }
-
-    /* 회원가입- 사장님 */
-    @GetMapping("joinCEO")
-    public String joinCEO(){ return "user/joinCEO"; }
-
-    @GetMapping("joinCEO2")
-    public String joinCEO2(){ return "user/joinCEO2"; }
-
-    /* 회원가입- 약관동의 */
-    @GetMapping("joinConfirm")
-    public String joinConfirm(){ return "user/joinConfirm"; }
 
 }
