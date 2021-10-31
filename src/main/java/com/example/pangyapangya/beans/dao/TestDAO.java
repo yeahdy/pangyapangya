dee06b3fd2dc7097d39706203003131348262dcb
han <jja4985@naver.com>
2021-11-01 03:14:10 +0900

    무한 스크롤 페이징 성공!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/com/example/pangyapangya/beans/dao/TestDAO.java
+++ src/main/java/com/example/pangyapangya/beans/dao/TestDAO.java
@@ -1,11 +1,26 @@
 package com.example.pangyapangya.beans.dao;
 
+import com.example.pangyapangya.beans.vo.TestingVO;
 import com.example.pangyapangya.mappers.TestMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class TestDAO {
     private final TestMapper mapper;
+
+    public List<TestingVO> read(){
+        return mapper.read();
+    }
+
+    public List<TestingVO> addData(int temp){
+        return mapper.addData(temp);
+    }
+
+    public int getTotal(){
+        return mapper.getTotal();
+    }
 }
