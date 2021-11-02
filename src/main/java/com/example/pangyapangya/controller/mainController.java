c16b5f249f55e3efbae8689b32afa36b769dc57e
yejine98 <yejine98@naver.com>
2021-11-02 10:59:09 +0900

    로그인(일반회원, 사장님) 완료 ※ 로그인 실패시 알림창 띄우기 실패

--- src/main/java/com/example/pangyapangya/controller/mainController.java
+++ src/main/java/com/example/pangyapangya/controller/mainController.java
@@ -1,12 +1,11 @@
 package com.example.pangyapangya.controller;
 
+import com.example.pangyapangya.beans.vo.CeoVO;
 import com.example.pangyapangya.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
 /*
