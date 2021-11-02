e0fe2919fad88925ff2eaa506f31a06dbb500ec5
sptm147 <sptm147@naver.com>
2021-11-02 09:37:45 +0900

    21.11.02 // 09:37

--- src/main/java/com/example/pangyapangya/controller/mainController.java
+++ src/main/java/com/example/pangyapangya/controller/mainController.java
@@ -1,5 +1,6 @@
 package com.example.pangyapangya.controller;
 
+import com.example.pangyapangya.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
