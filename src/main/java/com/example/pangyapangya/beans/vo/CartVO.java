889a2d1df872cef6e920378a8fe0b88042a58466
sptm147 <sptm147@naver.com>
2021-11-03 18:29:02 +0900

    21.11.03 // 18:28

--- src/main/java/com/example/pangyapangya/beans/vo/CartVO.java
+++ src/main/java/com/example/pangyapangya/beans/vo/CartVO.java
@@ -5,12 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class CartVO {
-    private Long cartNum;
+    private int cartNum;
     private String userId;
     private String breadName;
     private int priceOfBread;
     private int deliverCharge;
     private int breadCnt;
     private String breadImg;
-    private String ceoId;
 }
--- src/main/java/com/example/pangyapangya/mappers/ListMapper.java
+++ src/main/java/com/example/pangyapangya/mappers/ListMapper.java
@@ -1,15 +1,13 @@
-/*
 package com.example.pangyapangya.mappers;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-
+/*
 @Mapper
 public interface ListMapper {
     //  게시글 조회
     public
 }
-
 */
