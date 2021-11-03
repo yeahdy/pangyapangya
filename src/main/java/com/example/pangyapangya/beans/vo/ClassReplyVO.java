81f2c5423d03a2e865d34ab89bf7e5a782e08e65
lhj9472 <lhj9472@naver.com>
2021-11-03 17:54:51 +0900

    merge전 커밋

---
+++ src/main/java/com/example/pangyapangya/beans/vo/ClassReplyVO.java
@@ -0,0 +1,16 @@
+package com.example.pangyapangya.beans.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class ClassReplyVO {
+    private Long rno;
+    private Long bno;
+    private String content;
+    private String userid;
+    private String star;
+    private String reviewDate;
+
+}
