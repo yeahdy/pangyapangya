1b2bef76dd51fb525f801b9cdea9f081dd246a29
yejine98 <yejine98@naver.com>
2021-11-03 16:54:21 +0900

    아이디찾기(사장님) 완료, 비밀번호 찾기 + 변경 (일반회원) 완료

---
+++ src/main/java/com/example/pangyapangya/mappers/CEOMapper.java
@@ -0,0 +1,30 @@
+package com.example.pangyapangya.mappers;
+
+import com.example.pangyapangya.beans.vo.CeoVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CEOMapper {
+    // 아이디 중복검사
+    public int checkIdCEO (String ceoId);
+
+    // 회원가입
+    public void joinCEO(CeoVO ceoVO);
+
+    // 로그인
+    public int loginCEO (CeoVO ceoVO);
+
+    // 아이디 찾기
+    public List<CeoVO> idFindCEO (String phoneNum);
+
+    // 아이디 찾기 갯수
+    public int idFindCntCEO (String phoneNum);
+
+    //비밀번호 찾기: 아이디 유무검사
+    public int pwFindCEO (String ceoId);
+
+    // 회원정보 조회
+    public CeoVO ceoInfo (String ceoId);
+}
