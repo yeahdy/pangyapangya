c4f2136236892f7c42c0f792754238813a50a5da
han <jja4985@naver.com>
2021-11-03 16:01:25 +0900

    박살날까봐 무서워서 커밋

--- src/main/java/com/example/pangyapangya/controller/TestController.java
+++ src/main/java/com/example/pangyapangya/controller/TestController.java
@@ -1,24 +1,46 @@
 package com.example.pangyapangya.controller;
 
+import com.example.pangyapangya.services.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 @RequestMapping("/test/*")
 @RequiredArgsConstructor
 public class TestController {
+    private final TestService testService;
 
     @GetMapping("list")
-    public String list(){
+    public String list(Model model){
+        model.addAttribute("list", testService.read());
+        model.addAttribute("total", testService.getTotal());
         return "test/list";
     }
 
+//    @GetMapping({"read", "modify"})
+//    public void read(@RequestParam("bno") Long bno, Criteria criteria, Model model, HttpServletRequest request){
+//        String reqURI = request.getRequestURI();
+//        String reqType = reqURI.substring(reqURI.indexOf(request.getContextPath()) + 7);
+//        //read 요청 시 read 출력
+//        //modify 요청 시 modify 출력
+//        log.info("-------------------------------");
+//        log.info(reqType + " : " + bno);
+//        log.info("-------------------------------");
+//
+//        model.addAttribute("board", boardService.get(bno));
+//        model.addAttribute("criteria", criteria);
+//    }
     @GetMapping("read")
-    public String read(){return "test/read";}
+    public String read(@RequestParam("tno") Long tno, Model model){
+        model.addAttribute("item", testService.getRead(tno));
+        return "test/read";
+    }
 
     @GetMapping("readSuccess")
     public String readSuccess(){return "test/readSuccess";}
--- src/main/java/com/example/pangyapangya/services/TestService.java
+++ src/main/java/com/example/pangyapangya/services/TestService.java
@@ -1,11 +1,32 @@
 package com.example.pangyapangya.services;
 
+import com.example.pangyapangya.beans.dao.TestDAO;
+import com.example.pangyapangya.beans.vo.TestingVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class TestService {
+    private final TestDAO testDAO;
+
+    public List<TestingVO> read(){
+        return testDAO.read();
+    }
+
+    public List<TestingVO> addData(int temp){
+        return testDAO.addData(temp);
+    }
+
+    public int getTotal(){
+        return testDAO.getTotal();
+    }
+
+    public TestingVO getRead(Long tno){
+        return testDAO.getRead(tno);
+    }
 }
