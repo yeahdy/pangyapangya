4e217e1b7f5c73f5e73cecfd41af8a8b0e7d95b8
gomjiyun <rlawldbs7770@gmail.com>
2021-11-07 17:29:56 +0900

    Revert "leftDiv 경로 수정"
    This reverts commit f518a315

--- src/main/java/com/example/pangyapangya/controller/MypageController.java
+++ src/main/java/com/example/pangyapangya/controller/MypageController.java
@@ -1,59 +1,76 @@
-//package com.example.pangyapangya.controller;
-//
-//import com.example.pangyapangya.beans.dao.CartDAO;
-//import com.example.pangyapangya.beans.vo.CartVO;
-//import com.example.pangyapangya.services.CartService;
-//import com.example.pangyapangya.services.UserService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
-//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//import org.springframework.web.servlet.view.RedirectView;
-//import org.thymeleaf.model.IModel;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//@Controller
-//@Slf4j
-//@RequestMapping("/mypage/*")
-//@RequiredArgsConstructor
-//public class MypageController {
-//    private final UserService userService;
-//
-//    //mypage_user
-//
-//
-//    @GetMapping("order")
-//    public String order(){ return "mypage/order"; }
-//    @GetMapping("bread_review")
-//    public String bread_review(){ return "mypage/bread_review"; }
-//    @GetMapping("oneDayClass_review")
-//    public String oneDayClass_review(){ return "mypage/oneDayClass_review"; }
-//    @GetMapping("testing_review")
-//    public String testing_review(){ return "mypage/testing_review"; }
-//
-//    @GetMapping("modifyMyInfo")
-//    public String modifyMyInfo(String userId, Model model){
-//        model.addAttribute("userInfo",userService.userInfo("kjy1234"));
-//        return "mypage/modifyMyInfo";
-//    }
-//    /*@GetMapping("checkPassword")
-//    public String checkPassword(){ return "mypage/checkPassword"; }*/
-//    @GetMapping("breadOrderList")
-//    public String breadOrderList(){ return "mypage/breadOrderList"; }
-//    @GetMapping("oneDayClassList")
-//    public String oneDayClassList(){ return "mypage/oneDayClassList"; }
-//    @GetMapping("testingList")
-//    public String testingList(){ return "mypage/testingList"; }
-//    @GetMapping("myWriting_new")
-//    public String myWriting_new(){ return "mypage/myWriting_new"; }
-//    @GetMapping("checkPassword_new")
-//    public String checkPassword_new(){ return "mypage/checkPassword_new"; }
-//
-//}
+package com.example.pangyapangya.controller;
+
+import com.example.pangyapangya.beans.dao.CartDAO;
+import com.example.pangyapangya.beans.vo.CartVO;
+import com.example.pangyapangya.beans.vo.UserVO;
+import com.example.pangyapangya.services.CartService;
+import com.example.pangyapangya.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import org.thymeleaf.model.IModel;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+@RequestMapping("/mypage/*")
+@RequiredArgsConstructor
+public class MypageController {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    //mypage_user
+
+
+    @GetMapping("order")
+    public String order(){ return "mypage/order"; }
+    @GetMapping("bread_review")
+    public String bread_review(){ return "mypage/bread_review"; }
+    @GetMapping("oneDayClass_review")
+    public String oneDayClass_review(){ return "mypage/oneDayClass_review"; }
+    @GetMapping("testing_review")
+    public String testing_review(){ return "mypage/testing_review"; }
+
+    //회원정보 수정 전 비밀번호 검사
+    @GetMapping("checkPassword_new")
+    public String checkPassword_new(){
+        return "mypage/checkPassword_new"; }
+
+    @PostMapping("modifyMyInfo")
+    public String modifyMyInfo(Model model, HttpSession session, String pw){
+        String sessionU = (String)session.getAttribute("sessionU");
+        log.info("session 회원아이디: " + sessionU);
+        if(sessionU == null){
+            return "/user/login";
+        }
+        UserVO userInfo = userService.userInfo(sessionU);
+
+        //check_pw에서 비밀번호를 잘못 입력할 경우
+        //ajax 사용해서 틀렸을 때 alter 띄워주는거 가능할듯
+        //userInfo.getUserPw()
+        if (!passwordEncoder.matches(userInfo.getUserPw(), pw)){
+              return "mypage/checkPassword_new";
+        }
+        //비밀번호 받아야함.
+        model.addAttribute("userInfo",userInfo);
+        return "mypage/modifyMyInfo";
+    }
+    /*@GetMapping("checkPassword")
+    public String checkPassword(){ return "mypage/checkPassword"; }*/
+    @GetMapping("breadOrderList")
+    public String breadOrderList(){ return "mypage/breadOrderList"; }
+    @GetMapping("oneDayClassList")
+    public String oneDayClassList(){ return "mypage/oneDayClassList"; }
+    @GetMapping("testingList")
+    public String testingList(){ return "mypage/testingList"; }
+    @GetMapping("myWriting_new")
+    public String myWriting_new(){ return "mypage/myWriting_new"; }
+
+}
