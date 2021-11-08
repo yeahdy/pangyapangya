4e916518a00166f0897ce6aa7a46053dbf2954fd
han <jja4985@naver.com>
2021-11-09 03:04:08 +0900

    사진추가

--- src/main/java/com/example/pangyapangya/services/ClassCeoService.java
+++ src/main/java/com/example/pangyapangya/services/ClassCeoService.java
@@ -12,10 +12,12 @@ public interface ClassCeoService {
     public boolean modify(ClassCeoVO classCeoVO);
     public boolean remove(Long bno);
     public List<ClassCeoVO> getList(Criteria criteria);
+    public List<ClassCeoVO> getAllList();
     public int getTotal(Criteria criteria);
     public int myTotal(String ceoId);
     public CeoVO getCeo(String ceoId);
     public List<ClassCeoFileVO> getAttachList(Long bno);
+
 
     // 메인페이지 글 가져오기
     public List<ClassCeoVO> classList_main();
